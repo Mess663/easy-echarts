b6a807ddf55fa06f42a0767b8de8acb03248aa52
liukunhui <liukunhui@ecqun.com>
2023-03-06 15:28:54 +0800

    add eslint, refactor view entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.less'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./view/App";
+import "./main.less";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
---
+++ src/view/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./app.module.less";
+
+const App = () => {
+	return <div className={styles.container}>123</div>;
+};
+
+export default App;
