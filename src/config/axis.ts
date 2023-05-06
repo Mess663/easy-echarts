492fc9878b8504496084556fd74c3e56127dccd9
liukunhui <liukunhui@ecqun.com>
2023-05-06 10:53:44 +0800

    feat(Axis): 增加轴线位置

---
+++ src/config/axis.ts
@@ -0,0 +1,8 @@
+/**
+ * 坐标轴位置
+ * @doc https://echarts.apache.org/zh/option.html#xAxis.position
+ */
+export enum AxisPosition {
+    top = "top",
+    bottom = "bottom"
+}
--- src/option_forms/Axis/index.tsx
+++ src/option_forms/Axis/index.tsx
@@ -3,7 +3,13 @@ import css from "./index.module.less";
 import { OptionFormProps } from "../type";
 import { XAxis, YAxis } from "../../types/biz/option";
 import FormItem from "../../base/FormItem";
-import { Switch } from "antd";
+import { Dropdown, Select, Switch } from "antd";
+import { AxisPosition } from "../../config/axis";
+
+const positionMenu = [
+	{ value: AxisPosition.top, label: AxisPosition.top },
+	{ value: AxisPosition.bottom, label: AxisPosition.bottom },
+];
 
 const AxisForm = <T extends (XAxis | YAxis)>({ data, edit }: OptionFormProps<T>) => {
 	const onChange = (newData: Partial<XAxis>) => {
@@ -14,9 +20,21 @@ const AxisForm = <T extends (XAxis | YAxis)>({ data, edit }: OptionFormProps<T>)
 			<FormItem align title={"是否展示"} hash="xAxis.show">
 				<Switch
 					checked={data.show ?? true}
-					onChange={(flag) => {
-						onChange({ show: flag });
+					onChange={(show) => {
+						onChange({ show });
 					}}
+				/>
+			</FormItem>
+
+			<FormItem align title={"轴的位置"} hash="xAxis.position">
+				<Select
+					defaultValue={AxisPosition.bottom}
+					value={data.position ?? AxisPosition.bottom}
+					options={positionMenu}
+					onChange={(position) => {
+						onChange({ position });
+					}}
+					style={{ width: 90 }}
 				/>
 			</FormItem>
 		</div>
